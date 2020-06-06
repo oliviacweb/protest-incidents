344b13febf90e5358e05f6b7fa27df5731cb3b96
Olivia Webster <oliviacweb@gmail.com>
2020-06-06 16:21:26 -0400

    Created and console logged initial fecth of data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("https://raw.githubusercontent.com/2020PB/police-brutality/data_build/all-locations.json")
+            .then(res => res.json())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+
+       console.log(this.state)
+
+        return (
+            <div className="App">
+                <header className="App-header">
+
+                    <h1 className="App-title">Incident Reports</h1>
+                </header>
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
