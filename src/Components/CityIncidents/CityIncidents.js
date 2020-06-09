d29f17405bc86aa5a324556c587d84d8a16b5b12
Olivia Webster <oliviacweb@gmail.com>
2020-06-09 12:09:08 -0400

    Toggle saved

--- src/Components/CityIncidents/CityIncidents.js
+++ src/Components/CityIncidents/CityIncidents.js
@@ -9,20 +9,26 @@ class CityIncidents extends Component {
     constructor(props) {
         super(props);
       this.state = Object.assign({}, this.props.location.state);
+      this.toggleSave = this.toggleSave.bind(this);
+      //this in bind is actually object of city incident
 
     }
 
     isSaved(id) {
-      return this.state.savedStories[id]
+      return id in this.state.savedStories && this.state.savedStories[id]
+    }
 
+    toggleSave(event) {
+      const id = event.target.getAttribute('iid')
+      this.setState((prevState) => {
+        let newState = Object.assign({}, prevState)
+        newState.savedStories[id] = !this.isSaved(id)
+        return {newState}
+     })
     }
 
 
     render() {
-
-
-
-
         return (
            <div>
            <h2>Stories</h2>
@@ -30,8 +36,8 @@ class CityIncidents extends Component {
            {this.state.incidents.map((incident, index) =>
             <div key={index} className='incident-card'>
             {this.isSaved(incident.id) ?
-            <div className='star saved'>&#10034;</div> :
-            <div className='star'>&#10034;</div>}
+            <div onClick={this.toggleSave} iid={incident.id} className='star saved'>&#10034;</div> :
+            <div onClick={this.toggleSave} iid={incident.id} className='star'>&#10034;</div>}
             <h2>{incident.date}</h2>
             <h3>{incident.name}</h3>
             {incident.links.map((link, index2) => <li key={index2}><a href={link}>Click for more info</a></li>)}
